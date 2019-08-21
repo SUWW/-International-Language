2fdc045aaae662d154c94b6ef630e33eeb124c5d
su <su@sudeMacBook-Pro.local>
2019-08-21 12:32:20 +0800

    1

---
+++ InternationalLanguage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InternationalLanguage
+//
+//  Created by su on 2019/8/21.
+//  Copyright © 2019 su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
